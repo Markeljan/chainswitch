fe55b2452070d4d19a293e6538fa3441cfdc5b6f
Markeljan Sokoli <Markeljan19@gmail.com>
2024-04-03 15:33:34 -0400

    test

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ async function initializeApp() {
   setupRedirectFromUrl();
   setupEventListeners();
   updateSwitchNetworkButtonText();
+
+  const getStxToolsTokens = async () => {
+    const response = await fetch("https://api.stxtools.io/tokens");
+    return await response.json();
+  };
+
+  console.log("getStxToolsTokens", await getStxToolsTokens());
 }
 
 // Detect if the user is on a mobile device
